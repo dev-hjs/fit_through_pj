8d6fd6e1a50784065242463b27e950a4fcbe54ff
CTDKSKM <skmdldy001@gmail.com>
2023-06-28 10:07:28 +0900

    게시글 편집 ui 구현 (#9)

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,19 +1,19 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDC9AsrMHB0CPD6uX0HEZMZJOe-HXeKbOk",
-  authDomain: "react-news-feed-cba27.firebaseapp.com",
-  projectId: "react-news-feed-cba27",
-  storageBucket: "react-news-feed-cba27.appspot.com",
-  messagingSenderId: "277080772057",
-  appId: "1:277080772057:web:d059eab37e8493cfc85dc8",
+  apiKey: 'AIzaSyDC8cFneSkDavszFOKnqUBkzG3OFRIAQPE',
+  authDomain: 'teamproject-e3fc2.firebaseapp.com',
+  projectId: 'teamproject-e3fc2',
+  storageBucket: 'teamproject-e3fc2.appspot.com',
+  messagingSenderId: '833680902087',
+  appId: '1:833680902087:web:52a7e1d9a346248e15fbe2'
 };
 
 // Initialize Firebase
--- src/pages/PostEdit.jsx
+++ src/pages/PostEdit.jsx
@@ -1,7 +1,139 @@
-import React from 'react';
+import { collection, getDocs, query } from 'firebase/firestore';
+import React, { useEffect, useRef, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { styled } from 'styled-components';
+import { db } from '../firebase';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 
 const PostEdit = () => {
-  return <div>PostEdit</div>;
+  const [post, setPost] = useState([{ title: '', content: '', tags: [''] }]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const q = query(collection(db, 'test01'));
+      const querySnapshot = await getDocs(q);
+      const testPost = [];
+      querySnapshot.forEach((doc) => {
+        const data = {
+          id: doc.id,
+          ...doc.data()
+        };
+        testPost.push(data);
+      });
+      setPost(testPost);
+      setTestContents(testPost[0].content);
+    };
+    fetchData();
+  }, []);
+
+  const navigate = useNavigate();
+  const id = 1;
+  const [testContents, setTestContents] = useState('');
+  const onChangeContents = (contents) => {
+    setTestContents(contents);
+  };
+  console.log(testContents);
+  const ref = useRef(null);
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
+  // useEffect(() => {
+  //   return localStorage.setItem('test', JSON.stringify(testContents));
+  // }, [testContents]);
+  const fetchContents = JSON.parse(localStorage.getItem('test'));
+  return (
+    <>
+      <StEditDiv>
+        <StBtn
+          onClick={() => {
+            alert('저장완료');
+          }}
+        >
+          완료
+        </StBtn>
+        <StBtn
+          onClick={() => {
+            const check = window.confirm('아직 작성이 완료되지 않았습니다. 정말로 돌아가시겠습니까?');
+            if (check) {
+              navigate(`/posts/${id}`);
+            }
+          }}
+        >
+          돌아가기
+        </StBtn>
+      </StEditDiv>
+      <StInputDiv>
+        <StObject>제목</StObject>
+        <StContentArea contentEditable="true" onInput={(e) => {}} ref={ref}>
+          {post[0].title}
+        </StContentArea>
+      </StInputDiv>
+      <StInputDiv>
+        <StObject>태그</StObject>
+        <StContentArea contentEditable="true" onInput={(e) => {}}>
+          {post[0].tags.map((item, idx) => {
+            if (idx !== post[0].tags.length - 1) {
+              return <p key={idx}>{'#' + item + ','}</p>;
+            }
+            return <p key={idx}>{'#' + item}</p>;
+          })}
+        </StContentArea>
+      </StInputDiv>
+      <StInputDiv>
+        <StObject>내용</StObject>
+        <ReactQuill
+          style={{
+            width: '80%',
+            border: '1px solid gray',
+            borderRadius: '5px'
+          }}
+          value={testContents}
+          onChange={onChangeContents}
+        />
+      </StInputDiv>
+      {/* <ReactQuill value={fetchContents} /> */}
+    </>
+  );
 };
 
 export default PostEdit;
+
+const StEditDiv = styled.div`
+  width: 1200px;
+  height: 40px;
+  position: fixed;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  top: 0;
+`;
+const StInputDiv = styled.div`
+  display: flex;
+  width: 1000px;
+  justify-content: space-between;
+  align-items: center;
+  margin: 50px 0;
+`;
+const StObject = styled.p`
+  font-size: 35px;
+  font-weight: bold;
+`;
+const StContentArea = styled.div`
+  width: 80%;
+  min-height: ${({ content }) => (content ? '500px' : '22px')};
+  border: 1px solid gray;
+  border-radius: 5px;
+  display: flex;
+  padding-top: 5px;
+  padding-left: 3px;
+  overflow: hidden;
+  caret-color: #35c5f0;
+`;
+const StBtn = styled.button`
+  height: 30px;
+  border-radius: 10px;
+  background-color: transparent;
+  border: 1px solid #35c5f0;
+  margin: 0 5px;
+  cursor: pointer;
+`;
