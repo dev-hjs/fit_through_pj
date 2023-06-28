e69c0a1989e213ddfc75cbf3ccfcafe0f73d6053
leesoojin <leesoojin800@gmail.com>
2023-06-28 10:16:43 +0900

    게시글 작성 ui 구형 (#6)

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,7 +1,44 @@
-import React from 'react';
+import { collection, getDocs, query } from 'firebase/firestore';
+import React, { useEffect, useState } from 'react';
+import { db } from '../firebase';
 
 const PostDetail = () => {
-  return <div>PostDetail</div>;
+  const [test, setTest] = useState([
+    { text: '할 일1', id: 1 },
+    { text: '할 일2', id: 2 }
+  ]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const q = query(collection(db, 'test01'));
+      const querySnapshot = await getDocs(q);
+
+      const initialTest = [];
+
+      querySnapshot.forEach((doc) => {
+        const data = {
+          id: doc.id,
+          ...doc.data()
+        };
+        initialTest.push(data);
+      });
+      setTest(initialTest);
+    };
+    fetchData();
+  }, []);
+  console.log(test);
+
+  return (
+    <div>
+      {test.map((item) => (
+        <div key={item.id}>
+          <div>제목: {item.title}</div>
+          <div>태그: {item.tag}</div>
+          <div>내용: {item.content}</div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default PostDetail;
--- src/pages/PostRegist.jsx
+++ src/pages/PostRegist.jsx
@@ -1,7 +1,122 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { styled } from 'styled-components';
 
-const PostRegist = () => {
-  return <div>PostRegist</div>;
+const PostRegist = ({ closeModal }) => {
+  const [title, setTitle] = useState('');
+  const [tag, setTag] = useState('');
+  const [content, setConent] = useState('');
+
+  const handleAddTitle = (e) => {
+    setTitle(e.target.value);
+  };
+  const handleAddTag = (e) => {
+    setTag(e.target.value);
+  };
+  const handleAddContent = (e) => {
+    setConent(e.target.value);
+  };
+
+  const handleSave = () => {
+    const post = { title, tag, content };
+    const savedPosts = JSON.parse(localStorage.getItem('posts')) || [];
+    const updatedPosts = [...savedPosts, post];
+    localStorage.setItem('posts', JSON.stringify(updatedPosts));
+
+    closeModal();
+
+    setTitle('');
+    setTag('');
+    setConent('');
+  };
+
+  return (
+    <S.ModalContainer>
+      <S.ModalContent>
+        <div>
+          <S.InputGroup>
+            <S.InputLabel>제목:</S.InputLabel>
+            <S.ModalInput type="text" value={title} onChange={handleAddTitle} />
+          </S.InputGroup>
+          <S.InputGroup>
+            <S.InputLabel>태그:</S.InputLabel>
+            <S.ModalInput type="text" value={tag} onChange={handleAddTag} />
+          </S.InputGroup>
+          <S.InputGroup>
+            <S.InputLabel>내용:</S.InputLabel>
+            <S.ModalInputContent type="text" value={content} onChange={handleAddContent} />
+          </S.InputGroup>
+          <S.ModalButton onClick={handleSave}>저장</S.ModalButton>
+        </div>
+      </S.ModalContent>
+    </S.ModalContainer>
+  );
 };
 
 export default PostRegist;
+
+const S = {
+  ModalContainer: styled.div`
+    background-color: rgba(0, 0, 0, 0.4);
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `,
+
+  ModalContent: styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+
+    width: 400px;
+    height: 500px;
+
+    padding: 40px;
+
+    text-align: center;
+
+    background-color: rgb(255, 255, 255);
+    border-radius: 10px;
+    box-shadow: 0 2px 3px 0 rgba(34, 36, 38, 0.15);
+
+    transform: translateX(-50%) translateY(-50%);
+  `,
+
+  ModalButton: styled.button`
+    padding: 10px 20px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  `,
+
+  InputGroup: styled.div`
+    display: flex;
+    align-items: center;
+    margin-bottom: 10px;
+  `,
+
+  InputLabel: styled.label`
+    flex: 0 0 80px;
+    text-align: right;
+    margin-right: 10px;
+    margin-right: 10px;
+  `,
+
+  ModalInput: styled.input`
+    flex: 1;
+    height: 10px;
+    padding: 10px;
+  `,
+
+  ModalInputContent: styled.input`
+    flex: 1;
+    height: 60px;
+    padding: 10px;
+  `
+};
